67ce75a8be47b5305a1bcb82ebfcb199a846b9c2
Đinh Xuân Vũ <9569076+dxv2k@users.noreply.github.com>
2021-07-09 00:09:42 +0700

    Update train.sh to Colab version (dataset_dir)

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@ set -e
 export CUDA_VISIBLE_DEVICES=$1
 IMG_PER_GPU=$2
 
-TRAIN_DIR=${HOME}/models/pixel_link
+TRAIN_DIR=/content/pixel_link
 
 # get the number of gpus
 OLD_IFS="$IFS" 
@@ -13,13 +13,13 @@ IFS="$OLD_IFS"
 NUM_GPUS=${#gpus[@]}
 
 # batch_size = num_gpus * IMG_PER_GPU
-# BATCH_SIZE=`expr $NUM_GPUS \* $IMG_PER_GPU`
+BATCH_SIZE=`expr $NUM_GPUS \* $IMG_PER_GPU`
 
 #DATASET=synthtext
 #DATASET_PATH=SynthText
 
 DATASET=icdar2015
-DATASET_DIR=$HOME/dataset/pixel_link/icdar2015
+DATASET_DIR=/content/drive/MyDrive/icdar2015
 
 python train_pixel_link.py \
             --train_dir=${TRAIN_DIR} \
@@ -28,8 +28,7 @@ python train_pixel_link.py \
             --gpu_memory_fraction=-1 \
             --train_image_width=512 \
             --train_image_height=512 \
-            # --batch_size=${BATCH_SIZE}\
-            --batch_size=8\            
+            --batch_size=${BATCH_SIZE}\
             --dataset_dir=${DATASET_DIR} \
             --dataset_name=${DATASET} \
             --dataset_split_name=train \
@@ -50,5 +49,4 @@ python train_pixel_link.py \
             --dataset_split_name=train \
             --checkpoint_path=${CKPT_PATH} \
             --using_moving_average=1\
-            2>&1 | tee -a ${TRAIN_DIR}/log.log                        
-
+            2>&1 | tee -a ${TRAIN_DIR}/log.log           
